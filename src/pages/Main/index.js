a334461670bf2bbf98d8754694dc58d2e1cec41d
André Luiz <silva.ala82@gmail.com>
2024-01-11 15:58:41 -0300

    feat: implements list and delete repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,32 @@
 import React, { useState, useCallback } from "react";
-import { Conteiner, Form, SubmitButton } from "./styles";
-import { FaGithub, FaPlus } from "react-icons/fa";
+import { Conteiner, Form, SubmitButton, List, DeleteButton } from "./styles";
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from "react-icons/fa";
 import api from "../../services/api";
 
 export default function Main() {
   const [newRepo, setNewRepo] = useState("");
   const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = useCallback(
     (e) => {
       e.preventDefault();
       async function sumbit() {
-        const response = await api.get(`repos/${newRepo}`);
-        const data = {
-          name: response.data.full_name,
-        };
+        setLoading(true);
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+          console.log("response: ", response);
+          const data = {
+            name: response.data.full_name,
+          };
 
-        setRepositories(...repositories, data);
-        setNewRepo("");
+          setRepositories([...repositories, data]);
+          setNewRepo("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
       }
 
       sumbit();
@@ -28,6 +37,14 @@ export default function Main() {
   function handleInputChange(e) {
     setNewRepo(e.target.value);
   }
+
+  const handleDelete = useCallback(
+    (repo) => {
+      const find = repositories.filter((r) => r.name !== repo);
+      setRepositories(find);
+    },
+    [repositories]
+  );
 
   return (
     <Conteiner>
@@ -44,10 +61,30 @@ export default function Main() {
           onChange={handleInputChange}
         />
 
-        <SubmitButton>
-          <FaPlus color="#FFFFFF" />;
+        <SubmitButton Loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14} />
+          ) : (
+            <FaPlus color="#FFFFFF" />
+          )}
         </SubmitButton>
       </Form>
+
+      <List>
+        {repositories.map((repo) => (
+          <li key={repo.name}>
+            <span>
+              <DeleteButton onClick={() => handleDelete(repo.name)}>
+                <FaTrash size={14} />
+              </DeleteButton>
+              {repo.name}
+            </span>
+            <a href="">
+              <FaBars size={20} />
+            </a>
+          </li>
+        ))}
+      </List>
     </Conteiner>
   );
 }
