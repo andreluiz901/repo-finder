7e87a60895c9234f6c3c7b409da6af672a9ea7a0
André Luiz <silva.ala82@gmail.com>
2024-01-11 14:55:09 -0300

    feat: implements repo search

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Conteiner, Form, SubmitButton } from "./styles";
 import { FaGithub, FaPlus } from "react-icons/fa";
+import api from "../../services/api";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      async function sumbit() {
+        const response = await api.get(`repos/${newRepo}`);
+        const data = {
+          name: response.data.full_name,
+        };
+
+        setRepositories(...repositories, data);
+        setNewRepo("");
+      }
+
+      sumbit();
+    },
+    [newRepo, repositories]
+  );
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
   return (
     <Conteiner>
       <h1>
@@ -10,8 +36,13 @@ export default function Main() {
         Meus Repositórios
       </h1>
 
-      <Form>
-        <input type="text" placeholder="Adicionar repositórios" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositórios"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
         <SubmitButton>
           <FaPlus color="#FFFFFF" />;
